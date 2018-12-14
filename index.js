288d5f5583bac639360252c54cab86b979227bb4
Bakannee <45857178+Bakannee@users.noreply.github.com>
2018-12-14 22:34:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ var Kant = 0
 var lardfumé = 0
 var KamiSensei = 0
 var KaizukoInaha = 0
+var total = 0
+var pourcent = 0
 
 var banword = ['mouillé',
               'mouillée',
@@ -51,7 +53,10 @@ var banword = ['mouillé',
               'pk',
               'bambou',
               'cyprine',
-              'XD'
+              'XD',
+              'cochonne',
+              'kokine',
+              'penis'
               ]
 
 bot.on('ready', function(){
@@ -64,9 +69,8 @@ bot.on('message', function (message){
     var str = message.content
     var words = str.split(' ')
     for (var a = 0; a < 30 ; a++){
-        for (var i = 0; i < 43 ; i++){
+        for (var i = 0; i < 46 ; i++){
             if (words[a] === banword[i]) {
-               // console.log(words[a])
                 message.channel.send('Gotto is watching you : "Vous me dégoûtez." :expressionless: ')
                  if (sender === 'Bakann')
                                      Bakann++
@@ -85,7 +89,7 @@ bot.on('message', function (message){
 
                                 return
             }
-            if (words[a] === 'Ruby' && words[a+1] === 'Rose'){
+            if ((words[a] === 'Ruby' && words[a+1] === 'Rose') || (words[a] === 'X' && words[a+1] === 'D') ){
                 message.channel.send('Gotto is watching you : "Vous me dégoûtez." :expressionless: ')
                 if (sender === 'Bakann')
                      Bakann++
@@ -105,6 +109,46 @@ bot.on('message', function (message){
                 return
             }
         }
+    }
+
+    total = Bakann + retard + KamiSensei + KaizukoInaha + lardfumé + Kant + bidolo1
+
+    if (str === '!%'){
+        if (sender === 'Bakann'){
+            pourcent = 0
+            pourcent = (Bakann/total)*100
+            message.channel.send(pourcent+'%')
+        }
+        if (sender === 'lard fumé'){
+                    pourcent = 0
+                    pourcent = (lardfumé/total)*100
+                    message.channel.send(pourcent+'%')
+                }
+                if (sender === 'bidolo1'){
+                            pourcent = 0
+                            pourcent = (bidolo1/total)*100
+                            message.channel.send(pourcent+'%')
+                        }
+                        if (sender === 'Kami-Sensei'){
+                                    pourcent = 0
+                                    pourcent = (KamiSensei/total)*100
+                                    message.channel.send(pourcent+'%')
+                                }
+                                if (sender === 'retard'){
+                                            pourcent = 0
+                                            pourcent = (retard/total)*100
+                                            message.channel.send(pourcent+'%')
+                                        }
+                                        if (sender === 'Kant'){
+                                                    pourcent = 0
+                                                    pourcent = (Kant/total)*100
+                                                    message.channel.send(pourcent+'%')
+                                                }
+                                                if (sender === 'Kaizuko-Inaha'){
+                                                            pourcent = 0
+                                                            pourcent = (KaizukoInaha/total)*100
+                                                            message.channel.send(pourcent+'%')
+                                                        }
     }
 
     if (str === '!stats'){
@@ -151,6 +195,14 @@ bot.on('message', function (message){
                                            message.channel.send('Points de saletés : ')
         message.channel.send(lardfumé)
         }
+
+
+       if (str === '!help'){
+            message.channel.send('!% : Pour afficher votre pourcentage de saleté parmi la plèbe')
+            message.channel.send('!stats : Pour afficher le nombre de vos points de saleté')
+            message.channel.send('Vous pouvez ajouter le pseudo de la personne que vous souhaitez analyser derrière les commandes précendentes. ex : !stats retard')
+            }
+
 })
 
 bot.login(process.env.TOKEN)
